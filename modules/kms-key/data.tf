227dc6d05c53e1064cf60e5e398d8d18f89cf486
George Williams <williamsgeorge950@gmail.com>
2025-04-11 00:40:13 +0000

    modified the kms module

--- modules/kms-key/data.tf
+++ modules/kms-key/data.tf
@@ -1,13 +1,7 @@
 data "aws_iam_policy_document" "this" {
   statement {
-    effect = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = ["*"] # We'll restrict this better later when attaching to resources
-    }
-
-    actions = [
+    effect    = "Allow"
+    actions   = [
       "kms:Encrypt",
       "kms:Decrypt",
       "kms:ReEncrypt*",
@@ -15,6 +9,24 @@ data "aws_iam_policy_document" "this" {
       "kms:DescribeKey"
     ]
 
-    resources = ["*"]
+    # Restricting access to the specific IAM role (Uploader, Viewer, Admins)
+    principals {
+      type        = "AWS"
+      identifiers = [
+        # Replace these with actual IAM Role ARNs that need KMS access
+        aws_iam_role.uploader.arn,
+        aws_iam_role.viewer.arn
+      ]
+    }
+
+    
+    resources = [
+      "arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:key/${aws_kms_key.this.key_id}"
+    ]
   }
 }
+
+# 
+# data "aws_caller_identity" "current" {}
+
+# data "aws_region" "current" {}
