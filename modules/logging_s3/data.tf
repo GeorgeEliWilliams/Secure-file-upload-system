4286ab56fa3a1774e1275ac25465fec22d4c31be
George Williams <williamsgeorge950@gmail.com>
2025-04-14 07:19:44 +0000

    edited s3 logging policy

--- modules/logging_s3/data.tf
+++ modules/logging_s3/data.tf
@@ -1,6 +1,8 @@
+data "aws_caller_identity" "current" {}
+
 data "aws_iam_policy_document" "this" {
   statement {
-    sid    = "AllowS3LoggingAccess"
+    sid    = "AllowS3AccessLogs"
     effect = "Allow"
 
     principals {
@@ -15,5 +17,49 @@ data "aws_iam_policy_document" "this" {
     resources = [
       "${aws_s3_bucket.this.arn}/*"
     ]
+  }
+
+  # Statement 2: Allow CloudTrail to check the bucket's ACL
+  statement {
+    sid    = "AllowCloudTrailAclCheck"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetBucketAcl"
+    ]
+
+    resources = [
+      aws_s3_bucket.this.arn
+    ]
+  }
+
+  # Statement 3: Allow CloudTrail to write logs to a specific path in the bucket
+  statement {
+    sid    = "AllowCloudTrailWrite"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:PutObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.this.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
   }
 }
